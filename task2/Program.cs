7e34a38ef244e80867daa89ea247acc91145a671
unknown <hurina.irina@mail.ru>
2022-10-06 21:23:53 +0300

    промежуточное сохранение

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.WriteLine("Введите первое число:");
+string firstNumber = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+string secondNumber = Console.ReadLine();
+Console.WriteLine("Введите третье число:");
+string thirdNumber = Console.ReadLine();
+
+int Num1=int.Parse(firstNumber);
+int Num2=int.Parse(secondNumber);
+int Num3=int.Parse(thirdNumber);
